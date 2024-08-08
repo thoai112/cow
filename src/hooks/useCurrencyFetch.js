76c2166611ce5999a96db0b6404bc8cb76a1b6e6
thoai112 <thoailata1000@gmail.com>
2024-08-09 01:54:01 +0700

    fix api

--- src/hooks/useCurrencyFetch.js
+++ src/hooks/useCurrencyFetch.js
@@ -1,46 +1,49 @@
-import { useEffect, useState } from 'react';
-import dataJson from '../assets/data/currencies.json';
+import { useEffect, useState } from "react";
+import dataJson from "../assets/data/currencies.json";
 
 const useCurrencyFetch = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const mData = Object.entries(dataJson).map(([code, details]) => ({
+  let mData = Object.entries(dataJson).map(([code, details]) => ({
     code,
     ...details,
   }));
 
   async function fetchCurrencyData(code) {
     const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${code.toLowerCase()}.json`;
-    
+
     try {
-        const response = await fetch(url);
+      const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-        const responseData = await response.json();
-        // const price = responseData.data.defaultPair.price;
-        // const timestamp = new Date(responseData.data.timestamp).toLocaleString();
-        const time = responseData.data.date;
-        const price = responseData.data[code.toLowerCase()].vnd;
+      const responseData = await response.json();
+      // const price = responseData.data.defaultPair.price;
+      // const timestamp = new Date(responseData.data.timestamp).toLocaleString();
+      const time = responseData.date;
+      const price = responseData[code.toLowerCase()].vnd;
 
-        // Update datacurrency with price and timestamp
-        mData = mData.map(item => 
-            item.code === code ? { ...item, priceVND: price, timeUpdate: time } : item
-        );
-
-    } catch (error) {
-        console.error(`Error fetching currency data for ${code}:`, error);
+      // Update datacurrency with price and timestamp
+      mData = mData.map((item) =>
+        item.code === code
+          ? { ...item, priceVND: price, timeUpdate: time }
+          : item
+      );
+} catch (error) {
+      console.error(`Error fetching currency data for ${code}:`, error);
     }
-}
+  }
 
   async function updateCurrencyData() {
     try {
       setLoading(true);
-      const updatedData = await Promise.all(mData.map(({ code }) => fetchCurrencyData(code)));
+      const updatedData = await Promise.all(
+        mData.map(({ code }) => fetchCurrencyData(code))
+      );
       console.log("Updated Data:", updatedData); // Log the updated data
       setData(updatedData);
       setLoading(false);
@@ -53,7 +56,7 @@ const useCurrencyFetch = () => {
 
   useEffect(() => {
     updateCurrencyData();
-  },[]);
+  }, []);
 
   return {
     data,
