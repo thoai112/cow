c32083ed50ebe89afb4e49d21b4a311d343d7c4a
thoai112 <thoailata1000@gmail.com>
2024-08-09 02:05:15 +0700

    add Table

--- src/shared/Table.jsx
+++ src/shared/Table.jsx
@@ -9,98 +9,96 @@ import {
   // getFilteredRowModel,
   // getGroupedRowModel,
 } from "@tanstack/react-table";
-import { API_URL } from '../utils/config';
 import useCurrencyFetch from '../hooks/useCurrencyFetch';
 const Table = () => {
-  // const [sorting, setSorting] = useState([]);
-  // const [filters, setFilters] = useState({
-  //   code: "",
-  //   numToBasic: "",
-  //   minorSingle: "",
-  //   priceVND: [0, 1000000000],
-  // });
-  // const [visibleColumns, setVisibleColumns] = useState({
-  //   code: true,
-  //   numToBasic: true,
-  //   minorSingle: true,
-  //   priceVND: true,
-  // });
+  const [sorting, setSorting] = useState([]);
+  const [filters, setFilters] = useState({
+    code: "",
+    numToBasic: "",
+    minorSingle: "",
+    priceVND: [0, 1000000000],
+  });
+  const [visibleColumns, setVisibleColumns] = useState({
+    code: true,
+    numToBasic: true,
+    minorSingle: true,
+    priceVND: true,
+  });
   const { data: mData, loading, error } = useCurrencyFetch();
-  console.log(mData);
+ 
+  const filteredData = useMemo(() => {
+    return mData.filter((item) => {
+      const matchesCode =
+        filters.code === "" ||
+        item.name.toLowerCase().includes(filters.code.toLowerCase());
+      const matchesnumToBasic =
+        filters.numToBasic === "" ||
+        item.numToBasic
+          .toLowerCase()
+          .includes(filters.numToBasic.toLowerCase());
+      const matchesminorSingle =
+        filters.minorSingle === "" ||
+        item.minorSingle
+          .toLowerCase()
+          .includes(filters.minorSingle.toLowerCase());
+      const matchespriceVND =
+        item.priceVND >= filters.priceVND[0] &&
+        item.priceVND <= filters.priceVND[1];
+      return (
+        matchesCode &&
+        matchesnumToBasic &&
+        matchesminorSingle &&
+        matchespriceVND
+      );
+    });
+  }, [mData, filters]);
 
-  // const filteredData = useMemo(() => {
-  //   return mData.filter((item) => {
-  //     const matchesCode =
-  //       filters.code === "" ||
-  //       item.name.toLowerCase().includes(filters.code.toLowerCase());
-  //     const matchesnumToBasic =
-  //       filters.numToBasic === "" ||
-  //       item.numToBasic
-  //         .toLowerCase()
-  //         .includes(filters.numToBasic.toLowerCase());
-  //     const matchesminorSingle =
-  //       filters.minorSingle === "" ||
-  //       item.minorSingle
-  //         .toLowerCase()
-  //         .includes(filters.minorSingle.toLowerCase());
-  //     const matchespriceVND =
-  //       item.priceVND >= filters.priceVND[0] &&
-  //       item.priceVND <= filters.priceVND[1];
-  //     return (
-  //       matchesCode &&
-  //       matchesnumToBasic &&
-  //       matchesminorSingle &&
-  //       matchespriceVND
-  //     );
-  //   });
-  // }, [mData, filters]);
+  //Memoized columns based on visibleColumns state
+  const columns = useMemo(
+    () =>
+      [
+        {
+          header: "Code",
+          accessorKey: "Code",
+          footer: "Code",
+        },
+        {
+          header: "Basic Unit",
+          accessorKey: "numToBasic",
+          footer: "Basic Unit",
+        },
+        {
+          header: "Fractional Unit",
+          accessorKey: "minorSingle",
+          footer: "Fractional Unit",
+        },
+        {
+          header: "Price VND",
+          accessorKey: "priceVND",
+          footer: "Price VND",
+        },
+      ].filter((column) => visibleColumns[column.accessorKey]),
+    [visibleColumns]
+  );
 
-  // Memoized columns based on visibleColumns state
-  // const columns = useMemo(
-  //   () =>
-  //     [
-  //       {
-  //         header: "Code",
-  //         accessorKey: "Code",
-  //         footer: "Code",
-  //       },
-  //       {
-  //         header: "Basic Unit",
-  //         accessorKey: "numToBasic",
-  //         footer: "Basic Unit",
-  //       },
-  //       {
-  //         header: "Fractional Unit",
-  //         accessorKey: "minorSingle",
-  //         footer: "Fractional Unit",
-  //       },
-  //       {
-  //         header: "Price VND",
-  //         accessorKey: "priceVND",
-  //         footer: "Price VND",
-  //       },
-  //     ].filter((column) => visibleColumns[column.accessorKey]),
-  //   [visibleColumns]
-  // );
-
-  // // React Table hook setup
-  // const table = useReactTable({
-  //   data: filteredData,
-  //   columns,
-  //   getCoreRowModel: getCoreRowModel(),
-  //   getPaginationRowModel: getPaginationRowModel(),
-  //   getSortedRowModel: getSortedRowModel(),
-  //   // getFilteredRowModel: getFilteredRowModel(),
-  //   // getGroupedRowModel: getGroupedRowModel(), // Include getGroupedRowModel for grouping
-  //   state: {
-  //     sorting,
-  //   },
-  //   onSortingChange: setSorting,
-  // });
+  // React Table hook setup
+  const table = useReactTable({
+    data: filteredData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    // getFilteredRowModel: getFilteredRowModel(),
+    // getGroupedRowModel: getGroupedRowModel(), // Include getGroupedRowModel for grouping
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+  });
 
   return (
     <table>
-      {/* <thead className="thead-dark">
+      <thead className="thead-dark">
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
@@ -130,7 +128,7 @@ const Table = () => {
             ))}
           </tr>
         ))}
-      </tbody> */}
+      </tbody>
     </table>
   );
 };
